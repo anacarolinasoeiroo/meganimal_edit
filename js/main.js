0f4e8712bf8e85eb13f0b553501b32f36e3d4ff1
anacarolinasoeiroo <anacarolinasoeiro@live.com.pt>
2021-09-15 17:32:33 +0100

    done homepage

--- js/main.js
+++ js/main.js
@@ -8,3 +8,39 @@ $(document).ready(function(){
         arrows : false,
     });
   });
+//marcas
+  $('.marcas').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
